5c058b4f4d88fec1ff8a260c1065a627ce52d5d6
nian jian <another.nianj@gmail.com>
2016-02-14 16:36:27 +0800

    grid version 1.0 done
    sorting,filter, active/deactive column, headclick

--- src/component/grid/grid.jsx
+++ src/component/grid/grid.jsx
@@ -3,33 +3,69 @@ var classnames = require('classnames');
 
 module.exports = React.createClass({
     propTypes: {
-        gridList: React.propTypes.array.isRequired,
-        gridColumn: React.propTypes.array,
-        headClickHandle: React.propTypes.func,
-        headDBClickHandle: React.propTypes.func,
+        gridList: React.PropTypes.array.isRequired,
+        gridColumn: React.PropTypes.array,
+        sortBy: React.PropTypes.oneOfType([React.PropTypes.string,React.PropTypes.func]),
+        filterBy: React.PropTypes.func,
+        headClickHandle: React.PropTypes.func,
+        headDBClickHandle: React.PropTypes.func,
     },
     displayName: 'Grid',
     _buildHeads: function(gridColumn,gridList){
         gridColumn = gridColumn || Object.keys(gridList[0]);
-
         return gridColumn.map((head)=>{
             if(typeof(head) === 'object')
-                return (<th className={classnames({['sort-${head.sort}']: head.sort })} onClick={this.onClick} onDoubleClick={this.onDoubleClick}>{head.isActive? head : ""}</th>);
+                return (<th key={head.name} className={classnames({['sort-${head.sort}']: head.sort, active: head.isActive })} onClick={this.onClick} onDoubleClick={this.onDoubleClick}>{head.isActive? head.text : ""}</th>);
             else
-                return (<th  onClick={this.onClick} onDoubleClick={this.onDoubleClick}>head</th>);
+                return (<th key={head} onClick={this.onClick} onDoubleClick={this.onDoubleClick}>{head}</th>);
         });
 
     },
-    _buildBody: function(gridList){
+    _buildRows: function(gridColumn,gridList){
+        var {sortBy,filterBy} = this.props,sortByFunciton;
+        gridColumn = gridColumn || Object.keys(gridList[0]);
+        gridList = gridList.slice(); // Make the sort and filter on a copy of original grid list.
 
-    },
+        if (sortBy) {
+            sortByFunciton = typeof(sortBy) === 'function'? sortBy : (a,b)=>{
+                var sort = sortBy.split('|');
+                if (sort[1] === 'ASC') {
+                    if (a[sort[0]] > b[sort[0]]) {
+                        return 1;
+                    }else if (a[sort[0]] == b[sort[0]]) {
+                        return 0;
+                    }else{
+                        return -1;
+                    }
+                }
+            };
+
+            gridList.sort(sortByFunciton);
+        }
+
+        if (filterBy) {
+            gridList = gridList.filter(filterBy);
+        }
+
+        return gridList.map((row)=>{
+            var encRow;
+
+            if (typeof(gridColumn[0]) === 'object') {
+                encRow = gridColumn.map((column)=>(<td key={column.name} className={classnames({active: column.isActive})}>{row[column.name]}</td>));
+            }else{
+                encRow = gridColumn.map((column)=>(<td key={column} >{row[column]}</td>));
+            }
+            return(<tr key={row.id}>{encRow}</tr>);
+        });
+    }, 
     render: function(){
         var {gridList, gridColumn, ...originProps} = this.props;
         var theads = this._buildHeads(gridColumn,gridList);
+        var trows = this._buildRows(gridColumn,gridList);
         return (
                 <table {...originProps}>
-                    <thead>{theads}</thead>
-                    <tbody></tbody>
+                    <thead><tr>{theads}</tr></thead>
+                    <tbody>{trows}</tbody>
                     <tfoot></tfoot>
                 </table>
         );
--- src/container/application/application.jsx
+++ src/container/application/application.jsx
@@ -3,6 +3,7 @@ var React = require('react'),
     connect = require('react-redux').connect,
 
     TimerButton = require('../../component/timerbutton/timerbutton.jsx'),
+    Grid = require('../../component/grid/grid.jsx'),
 
     userActions = require('../../action/user.js');
 
@@ -14,6 +15,12 @@ var Application = React.createClass({
         return (
             <div className="application">
                 <TimerButton countDownFrom={30} stateText="点击获取验证码" countDownText="请等待 " unitText=" 秒后重试" autoStart={true} />
+                <Grid
+                gridColumn={[{name:'name',text:'Name',isActive:true},{name:'star',text:'Star',isActive:true}]}
+                gridList={[{id:1,name:'react',star: 9999,type:'javascript'},{id:2,name:'django',star:4956,type:'javascript'},{id:3,name:'rrui',star:12,type:'javascript'}]}
+                sortBy='star|ASC'
+                filterBy={(row)=>row.star > 10}
+                />
             </div>
         );
     }
